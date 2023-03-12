6f0ab9e78286d5e6ded2987ed042d254a055779a
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2023-03-12 07:55:11 -0300

    Update nightwatch.conf.js

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -13,7 +13,7 @@ module.exports = {
     host: '127.0.0.1',
     port: 4444,
     cli_args: {
-      'webdriver.chrome.driver': require('~/home/runner/work/VueJs-GitFinder/VueJs-GitFinder').path
+      'webdriver.chrome.driver': require('chromedriver').path
     }
   },
 
