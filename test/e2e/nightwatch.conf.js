4b98fd1de998c91096dfd9a4c9cb1de04e742486
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2023-03-12 10:18:02 -0300

    js test

---
+++ test/e2e/nightwatch.conf.js
@@ -0,0 +1,48 @@
+require('babel-register')
+var config = require('../../config')
+
+// http://nightwatchjs.org/gettingstarted#settings-file
+module.exports = {
+  src_folders: ['test/e2e/specs'],
+  output_folder: 'test/e2e/reports',
+  custom_assertions_path: ['test/e2e/custom-assertions'],
+  
+  selenium: {
+    start_process: false,
+    server_path: require('selenium-server-standalone-jar').path,
+    host: '127.0.0.1',
+    port: 4445,
+    cli_args: {
+      'webdriver.chrome.driver': require('chromedriver').path
+    }
+  },
+
+  test_settings: {
+    default: {
+      launch_url : "http://www.google.com",
+      selenium_port: 4445,
+      selenium_host: '127.0.0.1',
+      silent: false,
+      globals: {
+        devServerURL: 'http://127.0.0.1:' + (process.env.PORT || config.dev.port)
+      },
+      
+    },
+
+    chrome: {
+      desiredCapabilities: {
+        browserName: 'chrome',
+        javascriptEnabled: true,
+        acceptSslCerts: true,
+      }
+    },
+
+    firefox: {
+      desiredCapabilities: {
+        browserName: 'firefox',
+        javascriptEnabled: true,
+        acceptSslCerts: true
+      }
+    }
+  }
+}
