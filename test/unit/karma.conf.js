eb202ffef56a88f867dab6ea4b503e50ca2bee99
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2023-03-13 10:05:17 -0300

    Update karma.conf.js

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -5,14 +5,16 @@
 
 var webpackConfig = require('../../build/webpack.test.conf')
 
+let chai = require('chai'); 
+
 module.exports = function karmaConfig (config) {
   config.set({
     // to run in additional browsers:
     // 1. install corresponding karma launcher
     //    http://karma-runner.github.io/0.13/config/browsers.html
     // 2. add it to the `browsers` array below.
-    browsers: ['PhantomJS'],
-    frameworks: ['mocha', 'sinon-chai', 'phantomjs-shim'],
+    browsers: ['Chrome', 'Firefox'],
+    frameworks: ['mocha', 'phantomjs-shim'],
     reporters: ['spec', 'coverage'],
     files: ['./index.js'],
     preprocessors: {
