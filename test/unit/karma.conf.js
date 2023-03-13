7ca69b757abb3d292efcf02bef3718fd37eb507f
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2023-03-13 12:01:16 -0300

    Update karma.conf.js

--- test/unit/karma.conf.js
+++ test/unit/karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function karmaConfig (config) {
       ]
     },
     //reporters: ['spec', 'coverage'],
-    reporters: ['progress', 'kjhtml'],
+    reporters: ['progress'],
     files: ['./index.js'],
     // preprocessors: {
     //   './index.js': ['webpack', 'sourcemap']
