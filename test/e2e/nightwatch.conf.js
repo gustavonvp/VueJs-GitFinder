25f7f16ce5f0b0d33f64d2e435f655d7a1a73b28
gustavonvp <54008916+gustavonvp@users.noreply.github.com>
2023-03-13 08:11:10 -0300

    new node

--- test/e2e/nightwatch.conf.js
+++ test/e2e/nightwatch.conf.js
@@ -7,6 +7,7 @@ module.exports = {
   src_folders: ['test/e2e/specs'],
   output_folder: 'test/e2e/reports',
   custom_assertions_path: ['test/e2e/custom-assertions'],
+  browsers: ['PhantomJS', 'PhantomJS_custom'],
   
   selenium: {
     start_process: true,
